afe7720912a2f287b17f1cc6d3b962e0cb754eb3
Kristjan <nigathr@gmail.com>
2023-06-18 22:38:51 +0200

    Tilføjede første session + lektier

---
+++ Lektie 1/lektie.js
@@ -0,0 +1,39 @@
+// Opsætningsfunktioner:
+function getValueOf(id){
+	return document.getElementById(id)?.value
+}
+
+function getFirstValueAsNumber() {
+	return +getValueOf("first-value")
+}
+
+function getSecondValueAsNumber() {
+	return +getValueOf("second-value")
+}
+
+function writeToElement(value) {
+	document.getElementById("result").innerHTML = value
+}
+
+// Opgaveeksempel:
+// lav en funktion 'writeAdditionOf', som lægger to tal sammen og udskriver resultatet vha. writeToElement()
+
+function writeAdditionOf(a, b) {
+	let result = a + b
+	writeToElement("Resultatet er " + result)
+}
+
+// Opgave 1:
+// lav en funktion 'writeSubtractionOf', som trækker tal fra hinanden, og udskriver resultatet vha. writeToElement()
+
+
+// Opgave 2:
+// lav en funktion 'writeMultiplicationOf', som ganger to tal med hinanden, og udskriver resultatet vha. writeToElement()
+
+
+// Opgave 3:
+// lav en funktion 'writeDivisionOf', som deler et tal med at andet, og udskriver resultatet vha. writeToElement()
+
+
+// Bonus opgave:
+// lav en funktion 'writePowerOf', som opløfter et tal i et andet, og udskriver resultatet vha. writeToElement()
---
+++ Session 1/opgave.js
@@ -0,0 +1,29 @@
+
+// Opgave 1
+function writeNameAndAge(name, age) {
+	console.log("Hej, mit navn er " + name)
+	console.log("Jeg er " + age + " år gammel")
+}
+
+writeNameAndAge("Anna", "56")
+writeNameAndAge("Lasse", "29")
+writeNameAndAge("Sanna", "22")
+console.log("")
+
+
+// Opgave 2
+function writeResult(x, y) {
+	let result = x + y
+	console.log("Resultatet er " + result)
+}
+
+writeResult(10, 20)
+console.log("")
+
+// Opgave 3
+function writeCalculation(x, y) {
+	let result = x + y 
+	console.log("Resultatat af " + x + " + " + y + " er " + result)
+} 
+
+writeCalculation(10, 20)
